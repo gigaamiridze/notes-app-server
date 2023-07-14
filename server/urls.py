a684cc277649fa1ec05c27c7a184972b970b3ac2
Giga Amiridze <gigaamiridze@gmail.com>
2023-07-14 08:37:43 +0400

    Added feature to get all notes

--- server/urls.py
+++ server/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.get_routes, name='routes'),
+    path('notes', views.get_notes, name='notes'),
 ]
--- server/views.py
+++ server/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from .serializers import NoteSerializer
+from .models import Note
 # Create your views here.
 
 @api_view(['GET'])
@@ -39,3 +40,11 @@ def get_routes(request):
     ]
 
     return Response(routes)
+
+
+@api_view(['GET'])
+def get_notes(request):
+     notes = Note.objects.all()
+     serializer = NoteSerializer(notes, many=True)
+
+     return Response(serializer.data)
